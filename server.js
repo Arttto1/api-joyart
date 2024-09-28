44c41c9670921d11e6abdd9a3cb45f2291b36a19
Arthur Nunes <arthurnunes10@live.com>
2024-09-28 02:25:36 -0300

    4 try

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
-  next(); // Passa para o próximo middleware ou rota
+  next(); // Passa para o próximo middleware ou rotas
 });
 
 app.post("/log", express.json(), (req, res) => {
